892de7718eb674f4da7bf262fefcf3b021d22a34
Zohaer Al Mahatab <133798007+priom-mahatab@users.noreply.github.com>
2023-12-26 14:56:13 +0600

    Add files via upload

---
+++ Movies.h
@@ -0,0 +1,20 @@
+#include <Movie.h>
+#include <vector>
+
+class Movies
+{
+private:
+    std::vector<Movie> movies;
+public:
+    Movies();
+    ~Movies();
+    
+    bool add_movie(std::string name, std::string rating, int watched);
+    
+    bool increment_watched (std::string name);
+    
+    void display() const;
+
+};
+
+#endif // _MOVIES_H_
